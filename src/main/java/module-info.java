44375635b5fe415c27bb51ed351c59b10bb554c3
Debayan Sutradhar <debayansutradhar3@gmail.com>
2020-10-14 22:41:40 +0530

    Added Theme System, necessary dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,7 @@
 module com.StreamPi.Util {
+    requires org.apache.commons.configuration2;
+    requires org.slf4j;
+
     exports com.StreamPi.Util.Version;
     exports com.StreamPi.Util.Exception;
     exports com.StreamPi.Util.Platform;
