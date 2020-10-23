46f184e3dcc7e00fbb5232b8e9c873264f1132ed
dubbadhar <debayansutradhar3@gmail.com>
2020-10-23 23:01:06 +0530

    imported ikonli for other modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,9 @@ module com.StreamPi.Util {
     requires org.apache.commons.configuration2;
     requires org.slf4j;
 
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.fontawesome5;
+
     exports com.StreamPi.Util.Version;
     exports com.StreamPi.Util.Exception;
     exports com.StreamPi.Util.Platform;
