b4b33587422ff0230b956f2441411064d6926c96
Debayan Sutradhar <debayansutradhar3@gmail.com>
2021-05-22 20:28:24 +0530

    Updated IOHelper unzip method to throw IOException instead of just Exception

--- src/main/java/com/stream_pi/util/iohelper/IOHelper.java
+++ src/main/java/com/stream_pi/util/iohelper/IOHelper.java
@@ -18,24 +18,26 @@ package com.stream_pi.util.iohelper;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
-public class IOHelper {
+public class IOHelper
+{
 
-    public static void unzip(InputStream inputStream, String destDir) throws Exception {
+    public static void unzip(InputStream inputStream, String destDir) throws IOException
+    {
         File dir = new File(destDir);
-        // create output directory if it doesn't exist
         if(!dir.exists()) dir.mkdirs();
         InputStream fis;
-        //buffer for read and write data to file
         byte[] buffer = new byte[1024];
 
         fis = inputStream;
             ZipInputStream zis = new ZipInputStream(fis);
             ZipEntry ze = zis.getNextEntry();
-            while(ze != null){
+            while(ze != null)
+            {
                 String fileName = ze.getName();
 
                 File newFile = new File(destDir + File.separator + fileName);
@@ -54,11 +56,9 @@ public class IOHelper {
                     }
                     fos.close();
                 }
-                //close this ZipEntry
                 zis.closeEntry();
                 ze = zis.getNextEntry();
             }
-            //close last ZipEntry
             zis.closeEntry();
             zis.close();
             fis.close();
