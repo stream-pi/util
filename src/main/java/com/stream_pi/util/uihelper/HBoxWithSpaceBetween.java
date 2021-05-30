f4794e9e556eec7622c9192e7bcc1eb097c2d658
Debayan Sutradhar <debayansutradhar3@gmail.com>
2021-05-30 08:52:23 +0530

    Added HBoxWithSpaceBetween

---
+++ src/main/java/com/stream_pi/util/uihelper/HBoxWithSpaceBetween.java
@@ -0,0 +1,23 @@
+package com.stream_pi.util.uihelper;
+
+import javafx.scene.Node;
+import javafx.scene.layout.HBox;
+
+public class HBoxWithSpaceBetween extends HBox
+{
+    public HBoxWithSpaceBetween(Node node1, Node node2, double spacing)
+    {
+        getChildren().addAll(
+                node1,
+                SpaceFiller.horizontal(),
+                node2
+        );
+
+        setSpacing(spacing);
+    }
+
+    public HBoxWithSpaceBetween(Node node1, Node node2)
+    {
+        this(node1, node2, 5.0);
+    }
+}
