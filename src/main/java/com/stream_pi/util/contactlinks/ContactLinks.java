5239d576ef633a76beff45a7b8c927902dd576f2
Debayan Sutradhar <debayansutradhar3@gmail.com>
2021-06-03 21:26:13 +0530

    Added ContactLinks

---
+++ src/main/java/com/stream_pi/util/contactlinks/ContactLinks.java
@@ -0,0 +1,29 @@
+package com.stream_pi.util.contactlinks;
+
+public class ContactLinks
+{
+    public static String getGitHub()
+    {
+        return "https://github.com/Stream-Pi";
+    }
+
+    public static String getDiscord()
+    {
+        return "https://discord.gg/BExqGmk";
+    }
+
+    public static String getWebsite()
+    {
+        return "https://stream-pi.com";
+    }
+
+    public static String getTwitter()
+    {
+        return "https://twitter.com/Stream_Pi";
+    }
+
+    public static String getMatrix()
+    {
+        return "https://matrix.to/#/!hTwUYZonUXThjkMhCD:matrix.org?via=matrix.org";
+    }
+}
