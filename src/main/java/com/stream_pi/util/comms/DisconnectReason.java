32acc3c2eb739171ae7bfae411a3ac7295d34e54
Debayan Sutradhar <debayansutradhar3@gmail.com>
2021-10-22 20:19:23 +0530

    update serialVersionUID

--- src/main/java/com/stream_pi/util/comms/DisconnectReason.java
+++ src/main/java/com/stream_pi/util/comms/DisconnectReason.java
@@ -2,9 +2,13 @@ package com.stream_pi.util.comms;
 
 import com.stream_pi.util.i18n.I18N;
 
-public enum DisconnectReason
+import java.io.Serializable;
+
+public enum DisconnectReason implements Serializable
 {
     COMM_STANDARD_MISMATCH;
+
+    private static final long serialVersionUID = 2039570019671976L;
 
     public String getMessage()
     {
