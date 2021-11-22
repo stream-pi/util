75fc3b0b76c5e8b3382ab62a74f21ebeff64f19f
Debayan Sutradhar <debayansutradhar3@gmail.com>
2021-11-22 22:24:09 +0530

    fix I18N bug

--- src/main/java/com/stream_pi/util/alert/StreamPiAlertButton.java
+++ src/main/java/com/stream_pi/util/alert/StreamPiAlertButton.java
@@ -20,23 +20,23 @@ import org.kordamp.ikonli.javafx.FontIcon;
 
 public class StreamPiAlertButton
 {
-    private final String i18nString;
+    private final String text;
     private final String fontIconCode;
 
-    public StreamPiAlertButton(String i18nString)
+    public StreamPiAlertButton(String text)
     {
-        this(i18nString, null);
+        this(text, null);
     }
 
-    public StreamPiAlertButton(String i18nString, String fontIconCode)
+    public StreamPiAlertButton(String text, String fontIconCode)
     {
-        this.i18nString = i18nString;
+        this.text = text;
         this.fontIconCode = fontIconCode;
     }
 
     public Button getButton()
     {
-        Button button = new Button(I18N.getString(i18nString));
+        Button button = new Button(text);
 
         if(fontIconCode != null)
         {
@@ -46,8 +46,8 @@ public class StreamPiAlertButton
         return button;
     }
 
-    public static final StreamPiAlertButton YES = new StreamPiAlertButton("alert.StreamPiAlertButtonDefaults.yes");
-    public static final StreamPiAlertButton NO = new StreamPiAlertButton("alert.StreamPiAlertButtonDefaults.no");
-    public static final StreamPiAlertButton OK = new StreamPiAlertButton("alert.StreamPiAlertButtonDefaults.ok");
-    public static final StreamPiAlertButton CANCEL = new StreamPiAlertButton("alert.StreamPiAlertButtonDefaults.cancel");
+    public static final StreamPiAlertButton YES = new StreamPiAlertButton(I18N.getString("alert.StreamPiAlertButtonDefaults.yes"));
+    public static final StreamPiAlertButton NO = new StreamPiAlertButton(I18N.getString("alert.StreamPiAlertButtonDefaults.no"));
+    public static final StreamPiAlertButton OK = new StreamPiAlertButton(I18N.getString("alert.StreamPiAlertButtonDefaults.ok"));
+    public static final StreamPiAlertButton CANCEL = new StreamPiAlertButton(I18N.getString("alert.StreamPiAlertButtonDefaults.cancel"));
 }
