b002e62215fc0293cd422726c2f50f84248295eb
Debayan Sutradhar <debayansutradhar3@gmail.com>
2022-01-04 09:51:49 +0530

    add Global Uncaught Exception Handler

---
+++ src/main/java/com/stream_pi/util/exception/GlobalUncaughtExceptionHandler.java
@@ -0,0 +1,35 @@
+/*
+ * Stream-Pi - Free, Open-Source, Modular, Cross-Platform and Programmable Macro Pad
+ * Copyright (C) 2019-2022 Debayan Sutradhar (rnayabed),  Samuel Quiñones (SamuelQuinones)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package com.stream_pi.util.exception;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class GlobalUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler
+{
+    final private Logger logger = Logger.getLogger(GlobalUncaughtExceptionHandler.class.getName());
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e)
+    {
+        logger.log(Level.SEVERE, "Uncaught exception occurred!", e);
+    }
+
+    public static void init()
+    {
+        Logger.getLogger(GlobalUncaughtExceptionHandler.class.getName()).severe("TEsttt");
+        Thread.setDefaultUncaughtExceptionHandler(new GlobalUncaughtExceptionHandler());
+    }
+}
